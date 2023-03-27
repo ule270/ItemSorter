a54bf431a1fa40b731a70f27d6716b1b7659475f
ule270 <u.le270@gmail.com>
2023-03-26 22:15:18 -0400

    completed.

--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -10,6 +10,6 @@ import java.util.Comparator;
 public class PriceComparator implements Comparator<Item> {
     @Override
     public int compare(Item o1, Item o2) {
-        return (int) (o1.getPrice()-(o2.getPrice()));
+        return (int) (o1.getPrice().compareTo(o2.getPrice()));
     }
 }
